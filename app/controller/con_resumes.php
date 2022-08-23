372163b4b1cacc0b3b0495cdcd78764958438806
massao000 <onepiece.4224y@icloud.com>
2022-08-23 19:41:07 +0900

    未着手

---
+++ app/controller/con_resumes.php
@@ -0,0 +1,11 @@
+<?php
+include_once "..\models\model.php";
+include_once "..\models\masters.php";
+include_once "..\models\user.php";
+include_once "function.php";
+
+$user = new User(); // ユーザー
+$departments = new Departments(); // 学科
+$majors = new Majors(); // 専攻
+
+?>
