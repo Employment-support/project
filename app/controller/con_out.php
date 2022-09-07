4d3eabf3067b379ea003b0f9fc3673329474f814
massao000 <onepiece.4224y@icloud.com>
2022-09-07 23:44:55 +0900

    cookie削除(簡単に)

--- app/controller/con_out.php
+++ app/controller/con_out.php
@@ -1,9 +1,18 @@
 <?php
 
-session_start();
-$_SESSION = array();
-//セッションの中身をすべて削除
-session_destroy();
-//セッションを破壊
+setcookie('user_id', '', time()-10); // 
+setcookie('user_name', '', time()-10); // 
+setcookie('user_name_hiragana', '', time()-10); // 
+setcookie('user_number', '', time()-10); // 
+setcookie('user_admin', '', time()-10); // 
+setcookie('user_gender', '', time()-10); // 
+setcookie('user_type_id', '', time()-10); // 
+setcookie('user_type', '', time()-10); // 
+setcookie('user_department_id', '', time()-10); // 
+setcookie('user_department', '', time()-10); // 
+setcookie('user_major_id', '', time()-10); // 
+setcookie('user_major', '', time()-10); // 
+
+echo 'cookie削除';
 
 // header('Location:index');
