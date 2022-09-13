c968692036f1c10ccdc5e666611d4ed56ece29ab
massao000 <onepiece.4224y@icloud.com>
2022-09-13 22:10:39 +0900

    ファイル読み込みの変更

--- app/controller/con_home.php
+++ app/controller/con_home.php
@@ -1,8 +1,21 @@
 <?php
-include_once "..\models\model.php";
-include_once "..\models\masters.php";
-include_once "..\models\user.php";
-session_start();
+include_once __DIR__ . "\..\models\model.php";
+include_once __DIR__ . "\..\models\masters.php";
+include_once __DIR__ . "\..\models\user.php";
+
+print_r($_COOKIE); // テスト用
+echo $_COOKIE['user_id']; // テスト用
+echo $_COOKIE['user_name']; // テスト用
+echo $_COOKIE['user_name_hiragana']; // テスト用
+echo $_COOKIE['user_number']; // テスト用
+echo $_COOKIE['user_admin']; // テスト用
+echo $_COOKIE['user_gender']; // テスト用
+echo $_COOKIE['user_type_id']; // テスト用
+echo $_COOKIE['user_type']; // テスト用
+echo $_COOKIE['user_department_id']; // テスト用
+echo $_COOKIE['user_department']; // テスト用
+echo $_COOKIE['user_major_id']; // テスト用
+echo $_COOKIE['user_major']; // テスト用
 
 $user = new User();
 $briefings = new Briefings();
