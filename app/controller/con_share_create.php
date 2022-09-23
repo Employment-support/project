274e4dae363f200b8f4ea444ab6a42a29e24b21b
massao000 <onepiece.4224y@icloud.com>
2022-09-24 00:40:41 +0900

    DB登録処理追加 複数ファイルの送信処理追加

--- app/controller/con_share_create.php
+++ app/controller/con_share_create.php
@@ -14,24 +14,49 @@ $departments = new Departments(); // 学科
 $majors = new Majors(); // 専攻
 
 
-// post送信か確認
+// post送信確認とDB保存処理
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if (isset($_FILES['upload_file'])) {
+    $save_name_list = [];
+    if (isset($_FILES['file'])) {
+        // print_r($_FILES['file']);
         // ファイルの名前にランダムの文字列の結合
-        $savefile = "../media/imgs/". uniqid(mt_rand(), true). '-'. $_FILES["upload_file"]["name"];
-        move_uploaded_file($_FILES["upload_file"]["tmp_name"], $savefile);
+        // s3 にアップできるように
+        // https://tech.gootablog.com/article/s3-php/
+        foreach ($_FILES['file']['error'] as $key => $error) {
+            $save_db_name = "../media/imgs/". uniqid(mt_rand(), true). '-'. $_FILES["file"]["name"][$key];
+            $savefile = __DIR__ . '/' . $save_db_name;
+            move_uploaded_file($_FILES["file"]["tmp_name"][$key], $savefile);
+
+            array_push($save_name_list, $save_db_name);
+        }
+
     } else {
-        $savefile = '';
+        $save_name_list = [];
     }
-    $title = $_POST[''];
-    $contents = $_POST[''];
-    $file_path = $savefile;
+    $title = $_POST['title'];
+    $contents = $_POST['text'];
     $user_id = $_COOKIE['user_id'];
+    $file_path = $save_name_list;
+    
+    // print_r($file_path);
+
     // 登録
-    // $shares->create($title, $contents, $user_id, $file_path);
-    // header('Location:/shares');
-} else {
-    echo "何もない";
+    $return_type = $shares->create($title, $contents, $user_id, $file_path);
+    
+    if ($return_type !== true) {
+        // print_r($save_name_list);
+        foreach ($save_name_list as $save_name) {
+            // echo $save_name;
+            $shares->fileCreate($return_type, $save_name);
+        }
+    }
+
+    if ($return_type) {
+        // echo '投稿成功';
+        header('Location:/share');
+    } else {
+        echo '投稿できていない';
+    }
 }
 
 // 学生以外だけが入れる処理
@@ -39,7 +64,7 @@ if (is_admin($_COOKIE['user_admin']) || is_teacher($_COOKIE['user_type'])){
     $date = date('Y-m-d');
     $major_lists = $majors->selectAll($majors::sqlSelectAll);
     $department_lists = $departments->selectAll($departments::sqlSelectAll);
-    // require_once "../views/.php";
+    require_once __DIR__ . "/../views/vie_create_share.php";
 } else {
     header('Location:/shares');
 }
@@ -47,7 +72,7 @@ if (is_admin($_COOKIE['user_admin']) || is_teacher($_COOKIE['user_type'])){
 ?>
 <!-- コンボックスサンプル -->
 <!-- 実際はviewsの方におく -->
-<select name="department" id="department">
+<!-- <select name="department" id="department">
     <?php foreach ($department_lists as $department_list):?>
     <option value="<?=$department_list['id']?>"><?=$department_list['department']?></option>
     <?php endforeach;?>
@@ -57,7 +82,7 @@ if (is_admin($_COOKIE['user_admin']) || is_teacher($_COOKIE['user_type'])){
     <?php foreach ($major_lists as $major_list):?>
     <option value="<?=$major_list['id']?>"><?=$major_list['major']?></option>
     <?php endforeach;?>
-</select>
+</select> -->
 
 
 <?php
