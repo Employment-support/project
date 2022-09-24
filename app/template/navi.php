67040abfade6759928b1ca28182962220a9cb7d1
ManatoKawa <105094712+ManatoKawa@users.noreply.github.com>
2022-09-24 14:56:15 +0900

    Add files via upload

--- app/template/navi.php
+++ app/template/navi.php
@@ -1,4 +1,4 @@
-<nav>
+<!-- <nav>
 	<ul>
 		<li><a href="#" class="logo">OCA学生就職サポート</a></li>
 		<li><a href="#">共有情報</a></li>
@@ -7,5 +7,16 @@
 		<li><a href="vie_create_portfolio.html">ポートフォリオ作成</a></li>
 		<li><a href="vie_login.html" class="contact">ログイン</a></li>
 	</ul>
-</nav>
+</nav> -->
 
+<nav>
+	<ul>
+		<li class="left"><a href="#" class="logo">OCA学生就職サポート</a></li>
+		<li><a href="vie_share_list.php">共有情報</a></li>
+		<li><a href="#">企業説明</a></li>
+		<li><a href="vie_resume_create.php">履歴書作成</a></li>
+		<li><a href="vie_create_portfolio.php">ポートフォリオ作成</a></li>
+		<li class="right"><a href="vie_login.php" class="contact">ログイン</a></li>
+	</ul>
+	<hr>
+</nav>
