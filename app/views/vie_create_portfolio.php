14f0063078db228a2315e2d1b2a279f10f4c18fc
massao000 <onepiece.4224y@icloud.com>
2022-09-29 02:26:53 +0900

    navi.php footer.htmlを正しく読み込むようにコード修正 foamタグの追加 idとnameを追加

--- app/views/vie_create_portfolio.php
+++ app/views/vie_create_portfolio.php
@@ -13,12 +13,16 @@
     </title>
 </head>
 <body>
-    <?php __DIR__ .include ("../template/navi.php"); ?>
-    <p><label for="image">画像投稿<br><input type="file" id="image" required></label></p>
-    <p><label for="url">URL<br><input type="text" style="width: 500px; height: 20px;" id="url" required></label></p>
-    <p><label for="title">タイトル<br><input type="text" style="width: 500px; height: 20px;" id="title" required></label></p>
-    <p><label for="text">文章<br><input type="text" style="width: 500px; height: 600px;" id="text" required></label></p>
-    <input class="blue" type="submit" value="投稿">
+    <?php include (__DIR__ ."/../template/navi.php"); ?>
+
+    <form action="" method="POST" enctype="multipart/form-data">
+
+        <p><label for="image">画像投稿<br><input type="file" id="image" name="image" required></label></p>
+        <p><label for="url">URL<br><input type="text" style="width: 500px; height: 20px;" id="url" name="url" required></label></p>
+        <p><label for="title">タイトル<br><input type="text" style="width: 500px; height: 20px;" id="title" name="title" required></label></p>
+        <p><label for="text">文章<br><input type="text" style="width: 500px; height: 600px;" id="text" name="text" required></label></p>
+        <input class="blue" type="submit" value="投稿">
+    </form>
+    <?php include (__DIR__ ."/../template/footer.html"); ?>
 </body>
-<?php __DIR__ .include ("../template/footer.html"); ?>
 </html>
