211b2df26aeb4935f53baa6574549f1077ab4ca4
ManatoKawa <105094712+ManatoKawa@users.noreply.github.com>
2022-09-30 14:40:08 +0900

    Add files via upload

--- app/template/navi.php
+++ app/template/navi.php
@@ -16,7 +16,7 @@
 		<li><a href="#">企業説明</a></li>
 		<li><a href="vie_resume_create.php">履歴書作成</a></li>
 		<li><a href="vie_create_portfolio.php">ポートフォリオ作成</a></li>
-		<li class="right"><a href="vie_login.php" class="contact">ログイン</a></li>
+		<li class="right"><a href="vie_login.php" class="blue">ログイン</a></li>
 	</ul>
 	<hr>
 </nav>
