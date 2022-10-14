3aa4f42ef2f976f50421db23d1e740f1f6f38160
massao000 <onepiece.4224y@icloud.com>
2022-10-15 01:38:00 +0900

    配列があるか判断コード追加　途中

--- app/controller/con_portfolio_list.php
+++ app/controller/con_portfolio_list.php
@@ -14,14 +14,22 @@ $sql = '';
 // cookieが存在するか確認しそれが学生なら処理がされる
 if (isset($_COOKIE['user_type']) && is_student($_COOKIE['user_type'])) {
     $sql = $portfolio::sqlSelectAll . ' WHERE user_id = ' . $_COOKIE['user_id'];
+    echo '学生'; // test
 } else if (isset($_GET['u'])) {
     // 学生以外が見るときにURLパラメータを使って表示だけさせる
     $sql = $portfolio::sqlSelectAll . ' WHERE user_id = ' . $_GET['u'];
+    echo '一般'; // test
 }
 
 if ($sql){
     $portfolio_lists = $portfolio->selectAll($sql);
+
     print_r($portfolio_lists);
+}
+
+// ポートフォリオがあれば処理
+if (!empty($portfolio_lists)) {
+    echo 'ある';
 }
 
 // viewsでtmp_pagination.phpを呼び出す
