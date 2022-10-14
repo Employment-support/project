ae8d603ed10d68464babd721399ecbbefdacb3ec
massao000 <onepiece.4224y@icloud.com>
2022-10-15 00:46:25 +0900

    一部修正

--- app/template/tmp_pagination.php
+++ app/template/tmp_pagination.php
@@ -1,7 +1,7 @@
 <div class="pagination">
     <!-- 戻る -->
     <?php if ($page >= 2): ?>
-        <a href="<?=$url?>?page=<?php echo($page - 1); ?>" class="page_feed">&laquo;</a>
+        <a href="<?=$url?>?page=<?= ($page - 1); ?>" class="page_feed">&laquo;</a>
     <?php else : ;?>
         <span class="first_last_page">&laquo;</span>
     <?php endif; ?>
@@ -9,16 +9,16 @@
     <?php for ($i = 1; $i <= $max_page; $i++) : ?>
         <?php if($i >= $page - $range && $i <= $page + $range) : ?>
             <?php if($i == $page) : ?>
-                <span class="now_page_number"><?php echo $i; ?></span>
+                <span class="now_page_number"><?= $i; ?></span>
             <?php else: ?>
-                <a href="?page=<?php echo $i; ?>" class="page_number"><?php echo $i; ?></a>
+                <a href="?page=<?= $i; ?>" class="page_number"><?= $i; ?></a>
             <?php endif; ?>
         <?php endif; ?>
     <?php endfor; ?>
 
     <!-- 進む -->
     <?php if($page < $max_page) : ?>
-        <a href="<?=$url?>?page=<?php echo($page + 1); ?>" class="page_feed">&raquo;</a>
+        <a href="<?=$url?>?page=<?= ($page + 1); ?>" class="page_feed">&raquo;</a>
     <?php else : ?>
         <span class="first_last_page">&raquo;</span>
     <?php endif; ?>
