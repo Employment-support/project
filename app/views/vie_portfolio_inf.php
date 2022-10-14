a684f52104abe76f8eef2dfde8a722bf68307651
massao000 <onepiece.4224y@icloud.com>
2022-10-15 01:37:15 +0900

    in

--- app/views/vie_portfolio_inf.php
+++ app/views/vie_portfolio_inf.php
@@ -111,7 +111,6 @@
 			</p>
 	  	</div>
 	</a>
-	
 	<a href="#" class="card">
 	  	<img class="card-image" src="https://cdn.pixabay.com/photo/2020/03/03/20/31/boat-4899802_640.jpg">
 		<p class="card-url">
@@ -129,15 +128,15 @@
 	  	</div>
 	</a>
 
-	<a href="#" class="card">
-    <div class="plus">
-      
-		<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-			<line x1="12" y1="5" x2="12" y2="19"></line>
-			<line x1="5" y1="12" x2="19" y2="12"></line>
-		</svg>
-    </div>
+	<a href="#" class="card-create">
+		<div class="plus">
+			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+				<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+				<line x1="12" y1="5" x2="12" y2="19"></line>
+				<line x1="5" y1="12" x2="19" y2="12"></line>
+			</svg>
+			<p>新たに追加</p>
+		</div>
 	</a>
 	
 </div>
