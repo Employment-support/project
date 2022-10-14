c2dd38861ec8eb10a352c077a27830a66083f557
massao000 <onepiece.4224y@icloud.com>
2022-10-15 01:04:19 +0900

    aタグで指定しいて、ほかのページに影響を与えるためclass"link"を追加

--- app/template/navi.php
+++ app/template/navi.php
@@ -12,10 +12,10 @@
 <nav>
 	<ul>
 		<li class="left"><a href="home.php" class="logo">OCA学生就職サポート</a></li>
-		<li><a href="vie_share_list.php">共有情報</a></li>
-		<li><a href="#">企業説明</a></li>
-		<li><a href="vie_resume_create.php">履歴書作成</a></li>
-		<li><a href="vie_portfolio_create.php">ポートフォリオ作成</a></li>
+		<li><a class="link" href="vie_share_list.php">共有情報</a></li>
+		<li><a class="link" href="#">企業説明</a></li>
+		<li><a class="link" href="vie_resume_create.php">履歴書作成</a></li>
+		<li><a class="link" href="vie_portfolio_create.php">ポートフォリオ作成</a></li>
 		<li class="right"><a href="vie_login.php" class="blue">ログイン</a></li>
 	</ul>
 	<hr>
