b02242f4981761a94fa2aca19872d72b0063eb85
massao000 <onepiece.4224y@icloud.com>
2022-10-19 23:45:45 +0900

    PHP処理の追加

--- app/views/vie_portfolio_inf.php
+++ app/views/vie_portfolio_inf.php
@@ -11,91 +11,110 @@
 </head>
 <body>
     <?php include (__DIR__ ."/../template/navi.php"); ?>
-<table>
-    <tr>
-        <td colspan="3">
-		<a href="/portfolio/create" class="card-create">
-		<div class="plus">
-			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-				<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-				<line x1="12" y1="5" x2="12" y2="19"></line>
-				<line x1="5" y1="12" x2="19" y2="12"></line>
-			</svg>
-			<p>新たに追加</p>
-		</div>
-	</a>
-        </td>
-    </tr>
-    <tr>
-        <td class="td33">
-		<a href="/portfolio/create" class="card-create">
-		<div class="plus">
-			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-				<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-				<line x1="12" y1="5" x2="12" y2="19"></line>
-				<line x1="5" y1="12" x2="19" y2="12"></line>
-			</svg>
-			<p>新たに追加</p>
-		</div>
-	</a>
-        </td>
-        <td class="td33"></td>
-        <td class="td33"></td>
-    </tr>
-</table>
+
 <!-- 作成初期画面書き込み後 -->
 <!-- https://shu-sait.com/css-gazou-text-yokonarabi/ -->
 
 <div class="wrapper-big">
-	<a href="#" class="card-big">
-	  	<div class="card-image-big">
-			<img src="../static/imgs/NoImg.png">
-			<div class="card-url-big">
-				<object>
-					<a href="https://qiita.com/fukamiiiiinmin/items/7412b21c6df5de31cab1">url</a>
-				</object>
-			</div>
-	  	</div>
-	  	<div class="card-box-big">
-			<h2 class="card-title-big">
-				カードタイトル
-			</h2>
-			<p class="card-description-big">
-				ここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入る
-			</p>
-	  	</div>
-	</a>
+	<? #データがあれば表示 ?>
+	<?php if(!empty($portfolio_lists)): ?>
+		<?php foreach ($portfolio_lists as $data): ?>
+			<?php if($data['top'] == 1):?>
+				<a href="#" class="card-big">
+					<div class="card-image-big">
+						<!-- <img class="card-image" src="https://cdn.pixabay.com/photo/2020/03/03/20/31/boat-4899802_640.jpg"> -->
+						<?php if ($data['img_path']):?>
+							<img class="card-image" src="<?= $data['img_path'] ?>">
+						<?php else: ?>
+							<img class="card-image" src="../static/imgs/NoImg.png">
+						<?php endif ?>
+						<div class="card-url-big">
+							<object>
+								<a href="https://qiita.com/fukamiiiiinmin/items/7412b21c6df5de31cab1">url</a>
+							</object>
+						</div>
+					</div>
+					<div class="card-box-big">
+						<h2 class="card-title-big">
+							カードタイトル
+						</h2>
+						<p class="card-description-big">
+							ここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入るここに内容が入る
+						</p>
+					</div>
+				</a>
+			<?php
+				$display = TRUE;
+				break; 
+			?>
+			<?php endif ?>
+		<?php endforeach ?>
+		<? #トップが無ければ作成作成カードの表示 ?>
+		<?php if(!$display && $see):?>
+		<table>
+			<tr>
+				<td colspan="3">
+				<a href="/portfolio/create" class="card-create">
+					<div class="plus">
+						<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+							<line x1="12" y1="5" x2="12" y2="19"></line>
+							<line x1="5" y1="12" x2="19" y2="12"></line>
+						</svg>
+						<p>新たに追加</p>
+					</div>
+				</a>
+				</td>
+			</tr>
+			<!-- <tr>
+				<td class="td33">
+				<a href="/portfolio/create" class="card-create">
+				<div class="plus">
+					<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+						<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+						<line x1="12" y1="5" x2="12" y2="19"></line>
+						<line x1="5" y1="12" x2="19" y2="12"></line>
+					</svg>
+					<p>新たに追加</p>
+				</div>
+				</a>
+				</td>
+				<td class="td33"></td>
+				<td class="td33"></td>
+			</tr> -->
+			</table>
+		<?php endif ?>
+	<?php endif ?>
   </div>
   
   <!-- https://pa-tu.work/t/7104 -->
   <div class="wrapper">
 	<? #データがあれば表示 ?>
 	<?php if(!empty($portfolio_lists)): ?>
-	<?php foreach ($portfolio_lists as $data): ?>
-	<!-- <a href="#" class="card"> -->
-	<a href="#" class="card">
-	  	<!-- <img class="card-image" src="https://cdn.pixabay.com/photo/2020/03/03/20/31/boat-4899802_640.jpg"> -->
-		<?php if ($data['img_path']):?>
-			<img class="card-image" src="<?= $data['img_path'] ?>">
-		<?php else: ?>
-			<img class="card-image" src="../static/imgs/NoImg.png">
-		<?php endif ?>
-		<p class="card-url">
-		  	<object>
-				<!-- <a href="https://qiita.com/fukamiiiiinmin/items/7412b21c6df5de31cab1">url</a> -->
-				<a href="<?= $data['item_url'] ?>"><?= $data['item_url'] ?></a>
-			</object>
-		</p>
-	  	<div class="card-box">
-			<h2 class="card-title">
-				<?= $data['title'] ?>
-			</h2>
-			<p class="card-description">
-			<?= $data['contents'] ?>
+		<?php foreach ($portfolio_lists as $data): ?>
+		<a href="#" class="card">
+			<!-- <img class="card-image" src="https://cdn.pixabay.com/photo/2020/03/03/20/31/boat-4899802_640.jpg"> -->
+			<?php if ($data['img_path']):?>
+				<img class="card-image" src="<?= $data['img_path'] ?>">
+			<?php else: ?>
+				<img class="card-image" src="../static/imgs/NoImg.png">
+			<?php endif ?>
+			<p class="card-url">
+				<object>
+					<!-- <a href="https://qiita.com/fukamiiiiinmin/items/7412b21c6df5de31cab1">url</a> -->
+					<a href="<?= $data['item_url'] ?>"><?= $data['item_url'] ?></a>
+				</object>
 			</p>
-	  	</div>
-	</a>
-	<?php endforeach ?>
+			<div class="card-box">
+				<h2 class="card-title">
+					<?= $data['title'] ?>
+				</h2>
+				<p class="card-description">
+				<?= $data['contents'] ?>
+				</p>
+			</div>
+		</a>
+		<?php endforeach ?>
 	<?php endif ?>
 	
 	<?php if($see):?>
