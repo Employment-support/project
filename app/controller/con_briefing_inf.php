92ab1c672d0ce09155404a979fb367c5cc50cf57
massao000 <onepiece.4224y@icloud.com>
2022-10-20 00:06:52 +0900

    SQLの変更

--- app/controller/con_briefing_inf.php
+++ app/controller/con_briefing_inf.php
@@ -5,18 +5,22 @@ include_once __DIR__ . "/../models/masters.php";
 
 
 $briefings = new Briefings(); // 企業説明会
+// $corporations = new Corporations(); // 企業ジャンル
 
 if (isset($_GET['id']) && is_numeric($_GET['id'])){
     // 編集するデータ
     $contents_id = $_GET['id'];
-    $briefing_data = $briefings->select($contents_id, $briefings::sqlSelect);
+
+    
+    $sql = 'SELECT * FROM briefings INNER JOIN corporations ON briefings.corporation_id = corporations.id WHERE briefings.id = ?';
+    $briefing_data = $briefings->select($contents_id, $sql);
 
     if (!$briefing_data) {
         header('Location:/briefing');
     }
-    print_r($briefing_data);
+    // print_r($briefing_data);
     
-// require_once __DIR__ . "/../views/.php";
+require_once __DIR__ . "/../views/vie_briefings_inf.php";
 } else {
     header('Location:/briefing');
 }
