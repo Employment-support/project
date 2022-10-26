52f7cd744da892d698c80e5f9fe84fd7fed23fa3
massao000 <onepiece.4224y@icloud.com>
2022-10-27 01:34:32 +0900

    cookie確認削除

--- app/controller/con_portfolio_list.php
+++ app/controller/con_portfolio_list.php
@@ -22,12 +22,27 @@ $sql = '';
 // 新規登録の表示
 $see = FALSE;
 // cookieが存在するか確認しそれが学生なら処理がされる
-if ($_COOKIE){
-    if (isset($_COOKIE['user_type']) && is_student($_COOKIE['user_type']) || is_admin($_COOKIE['user_type'])) {
-        $sql = $portfolio::sqlSelectAll . ' WHERE user_id = ' . $_COOKIE['user_id'];
-        $see = TRUE;
-        // echo '学生'; // test
-    }
+// cookieが存在するか確認しそれが学生なら処理がされる
+// if ($_COOKIE){
+//     if (isset($_COOKIE['user_type']) && is_student($_COOKIE['user_type']) || is_admin($_COOKIE['user_type'])) {
+//         $sql = $portfolio::sqlSelectAll . ' WHERE user_id = ' . $_COOKIE['user_id'];
+//         $see = TRUE;
+//         // echo '学生'; // test
+//     }
+// } else if (isset($_GET['u'])) {
+//     // 学生以外が見るときにURLパラメータを使って表示だけさせる
+//     $sql = $portfolio::sqlSelectAll . ' WHERE user_id = ' . $_GET['u'];
+//     $see = FALSE;
+//     // echo '一般'; // test
+// } else {
+//     $sql = $portfolio::sqlSelectAll . ' WHERE user_id = ' . 0;
+//     $see = FALSE;
+//     // echo 'ない'; // test
+// }
+if (isset($_COOKIE['user_type']) || is_student($_COOKIE['user_type']) || is_admin($_COOKIE['user_type'])) {
+    $sql = $portfolio::sqlSelectAll . ' WHERE user_id = ' . $_COOKIE['user_id'];
+    $see = TRUE;
+    // echo '学生'; // test
 } else if (isset($_GET['u'])) {
     // 学生以外が見るときにURLパラメータを使って表示だけさせる
     $sql = $portfolio::sqlSelectAll . ' WHERE user_id = ' . $_GET['u'];
@@ -38,6 +53,7 @@ if ($_COOKIE){
     $see = FALSE;
     // echo 'ない'; // test
 }
+// echo $sql;
 
 if ($sql){
     $portfolio_lists = $portfolio->selectAll($sql);
